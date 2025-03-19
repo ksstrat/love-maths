09f8aff995dacd70ed0e137ba444436c118ec3e0
ksstrat <qpvgrwku@students.codeinstitute.net>
2025-03-19 21:04:16 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+/*
+Functions i need:
+runGame()
+checkAnswer()
+calculateCorrectAnswer()
+incrementScore()
+incrementWrongAnswer()
+displayAdditionQuestion()
+displaySubtractQuestion()
+displayMultiplyQuestion()
+*/
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
